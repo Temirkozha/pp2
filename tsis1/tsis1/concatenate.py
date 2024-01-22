5c6ed58359a0c273df8dbf154005376e4c914e07
Temirkozha <156663683+Temirkozha@users.noreply.github.com>
2024-01-22 16:39:59 +0600

    Add files via upload

---
+++ tsis1/tsis1/concatenate.py
@@ -0,0 +1,32 @@
+#1
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#2
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+#3
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#4
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+#5
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+#6
+txt = "We are the so-called \"Vikings\" from the north."
